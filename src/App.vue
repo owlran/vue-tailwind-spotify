49e2d040dd4d13ab3207f0145334c0d31bc74157
owlran <owlran1088@gmail.com>
2020-03-20 08:52:46 +0800

    refactor: prototype

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,19 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+  <div id="app" class="max-w-full w-full h-screen overflow-hidden bg-gray-300">
+    <div class="flex flex-wrap h-full w-full">
+      <div class='h-full w-1/4 xl:w-1/6 bg-gray-900 text-gray-200'>
+      </div>
+      <main class='flex flex-col h-full w-3/4 xl:w-5/6 bg-gray-800 text-gray-200'>
+      </main>
+      <div id="nav">
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+      </div>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,52 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a :href="loginURL">Login</a>
+    <h2>Location</h2>
+    <span>{{ userData.country }}</span>
+    <h2>name</h2>
+    <span>{{ userData.display_name }}</span>
+    <h2>image</h2>
+    <img :src="userData.images[0].url"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import {
+  getUser,
+  getNewUser,
+  getNewRelease,
+} from '@/service/spotify';
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      userData: null,
+    };
+  },
   components: {
     HelloWorld,
+  },
+  computed: {
+    loginURL() {
+      return 'http://localhost:8890/login';
+    },
+  },
+  async mounted() {
+    try {
+      const res = await getUser();
+      console.log(res.data);
+      this.userData = res.data;
+      const res2 = await getNewUser();
+      console.log(res2);
+      const res3 = await getNewRelease();
+      console.log(res3);
+    } catch (e) {
+      console.log(e);
+    }
   },
 };
 </script>
