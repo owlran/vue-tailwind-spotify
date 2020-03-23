2dcbc5594a201519f221102ef9cd1915bd26a293
owlran <owlran1088@gmail.com>
2020-03-23 09:02:29 +0800

    chore(eslint): disable max length lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,8 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'max-len': 0
   },
   overrides: [
     {
