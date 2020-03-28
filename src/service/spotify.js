d528daea5f0dc6fcb3bd3188d3d2da32910f2c8e
owlran <owlran1088@gmail.com>
2020-03-28 15:01:48 +0800

    refactor: use local stoage to sotre tokens

--- src/service/spotify.js
+++ src/service/spotify.js
@@ -1,13 +1,74 @@
 import axios from 'axios';
 import getHashParams from '@/utils/';
 
+// TOKENS ******************************************************************************************
+const EXPIRATION_TIME = 3600 * 1000; // 3600 seconds * 1000 = 1 hour in milliseconds
+
+const setTokenTimestamp = () => window.localStorage.setItem('spotify_token_timestamp', Date.now());
+const setLocalAccessToken = (token) => {
+  setTokenTimestamp();
+  window.localStorage.setItem('spotify_access_token', token);
+};
+const setLocalRefreshToken = (token) => window.localStorage.setItem('spotify_refresh_token', token);
+const getTokenTimestamp = () => window.localStorage.getItem('spotify_token_timestamp');
+const getLocalAccessToken = () => window.localStorage.getItem('spotify_access_token');
+const getLocalRefreshToken = () => window.localStorage.getItem('spotify_refresh_token');
+
+// Refresh the token
+const refreshAccessToken = async () => {
+  try {
+    const { data } = await axios.get(`http://localhost:8890/refresh_token?refresh_token=${getLocalRefreshToken()}`);
+    const { access_token: accessToken } = data;
+    setLocalAccessToken(accessToken);
+    window.location.reload();
+    return;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
 export const getAccessToken = () => {
   const {
     access_token: accessToken,
-    // refresh_token: refreshToken,
+    error,
+    refresh_token: refreshToken,
   } = getHashParams();
 
-  return accessToken;
+  if (error) {
+    console.error(error);
+    refreshAccessToken();
+  }
+
+  // If token has expired
+  if (Date.now() - getTokenTimestamp() > EXPIRATION_TIME) {
+    console.warn('Access token has expired, refreshing...');
+    refreshAccessToken();
+  }
+
+  const localAccessToken = getLocalAccessToken();
+  const localRefreshToken = getLocalRefreshToken();
+
+  // If there is no REFRESH token in local storage, set it as `refresh_token` from params
+  if (!localRefreshToken || localRefreshToken === 'undefined') {
+    setLocalRefreshToken(refreshToken);
+  }
+
+  // If there is no ACCESS token in local storage, set it and return `access_token` from params
+  if (!localAccessToken || localAccessToken === 'undefined') {
+    setLocalAccessToken(accessToken);
+    return accessToken;
+  }
+
+  return localAccessToken;
+};
+
+export const token = getAccessToken();
+
+export const logout = () => {
+  window.localStorage.removeItem('spotify_token_timestamp');
+  window.localStorage.removeItem('spotify_access_token');
+  window.localStorage.removeItem('spotify_refresh_token');
+  window.location.reload();
 };
 
 export const accessToken = getAccessToken();
