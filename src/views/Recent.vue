cb9361d4b7974b72234277519a8f60734fba36ae
owlran <owlran1088@gmail.com>
2020-03-29 03:45:53 +0800

    refactor(Recent): add rencet played prototype

--- src/views/Recent.vue
+++ src/views/Recent.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="w-full bg-gray-500">
-    <p>Recent</p>
+  <div class="w-1/2 bg-gray-900 mx-auto">
+    <loader v-if="isLoading"></loader>
+    <div class="content" v-else>
+      <p>Recent</p>
+    </div>
+    <div>
+      <TracksList :tracksList="tracks"></TracksList>
+    </div>
   </div>
 </template>
+
+<script>
+import Loader from '@/components/Loader.vue';
+import { getRecentlyPlayed } from '@/service/spotify';
+import TracksList from '@/components/TracksList.vue';
+
+export default {
+  data() {
+    return {
+      isLoading: true,
+      recentPlayed: null,
+    };
+  },
+  computed: {
+    tracks() {
+      return this.recentPlayed && this.recentPlayed.items.map((item) => item.track);
+    },
+  },
+  components: {
+    TracksList,
+    Loader,
+  },
+  async mounted() {
+    const res = await getRecentlyPlayed();
+    this.recentPlayed = res.data;
+    this.isLoading = false;
+  },
+};
+
+</script>
