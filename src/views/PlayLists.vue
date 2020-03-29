11562b61f1988d81d2c361109ad6dafaa4109cb1
owlran <owlran1088@gmail.com>
2020-03-29 17:53:08 +0800

    refactor(PlayLists): add playlists prototype

--- src/views/PlayLists.vue
+++ src/views/PlayLists.vue
@@ -1,15 +1,23 @@
 <template>
-  <div class="w-1/2 bg-gray-500 mx-auto">
-    <p>Play lists</p>
-    <div v-if="lists">
-      <p v-for="(list, index) in lists" :key="index">
-        {{ list.name }}
-      </p>
+  <div class="w-1/2 mx-auto py-10">
+    <BaseHeader :title="'Play lists'"></BaseHeader>
+    <div v-if="lists" class="text-white grid grid-cols-2 lg:grid-cols-4 gap-4 mt-20">
+      <div v-for="(list, index) in lists" :key="index" class="flex flex-col justify-center items-center">
+        <img :src="list.images[0].url"/>
+        <p class="pt-2 text-sm text-center">
+          {{ list.name }}
+        </p>
+        <p class="text-gray-600 text-xs uppercase">
+          {{ list.tracks.total }} tracks
+        </p>
+        <!-- {{ list.id }} -->
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import BaseHeader from '@/components/BaseHeader.vue';
 import {
   getPlaylists,
 } from '@/service/spotify';
@@ -19,6 +27,9 @@ export default {
     return {
       playLists: null,
     };
+  },
+  components: {
+    BaseHeader,
   },
   computed: {
     lists() {
