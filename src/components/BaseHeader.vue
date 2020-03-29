097e2b9ce5c372e2be0dff93acd8f38d1db8e723
owlran <owlran1088@gmail.com>
2020-03-29 23:06:19 +0800

    refactor(BaseHeader): only display when slot exists

--- src/components/BaseHeader.vue
+++ src/components/BaseHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex flex-col md:flex-row justify-between">
     <h2 class="text-center text-white text-2xl">{{ title }}</h2>
-    <div class="text-white flex justify-around mt-10">
+    <div class="text-white flex justify-around mt-10" v-if="hasSlot">
       <slot name="buttons">
       </slot>
     </div>
@@ -11,5 +11,10 @@
 <script>
 export default {
   props: ['title'],
+  computed: {
+    hasSlot() {
+      return !!this.$slots.buttons;
+    },
+  },
 };
 </script>
